d389e7af9fb431253764bf31c525fc029a7f1ebe
@willye <willye@tencent.com>
2023-07-04 10:00:06 +0800

    feat: 🎸 基于Tauri脚手架搭建桌面端APP,集成Excalidraw网址

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { invoke } from "@tauri-apps/api/tauri";
+
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
